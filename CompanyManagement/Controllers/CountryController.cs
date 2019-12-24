8ce7c2cd625ab47b9aedd48fb73f716f67126904
mohammedzaman <mohamed_s_zaman@hotmail.co.uk>
2019-12-24 14:14:11 +0000

    Project Started

---
+++ CompanyManagement/Controllers/CountryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CompanyManagement.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CountryController : ControllerBase
+    {
+  
+
+        private readonly ILogger<CountryController> _logger;
+
+        public CountryController(ILogger<CountryController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            
+        }
+    }
+}
